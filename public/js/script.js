278183e4a69b494b7340329d674b725e4079d5be
Keiichi Lindley <digizn@gmail.com>
2016-05-23 18:20:00 -0600

    Clean up layout

--- public/js/script.js
+++ public/js/script.js
@@ -6,7 +6,7 @@ $(function(){
     e.preventDefault();
     var pageID = $('#pageID').val()
     $('#results').html('')
-    FB.api('/' + pageID + '/posts?fields=full_picture,type,name,likes.limit(1).summary(true),shares,comments.limit(1).summary(true)', function(feed) {
+    FB.api('/' + pageID + '/posts?fields=link,full_picture,type,name,likes.limit(1).summary(true),shares,comments.limit(1).summary(true)', function(feed) {
       populateResults(feed)
     });
   })
@@ -14,42 +14,62 @@ $(function(){
 
 function populateResults(results){
   var data = []
+  console.log(results.data);
   results.data.map((item) => {
     if (item.type !== 'status') {
-      data.push({
-        name: item.name || ''
+      var card = {
+          name: item.name || ''
         , type: item.type
         , img: item.full_picture
         , shares: item.shares ? item.shares.count : 0
         , likes: item.likes.summary ? item.likes.summary.total_count : 0
         , comments: item.comments.summmary ? item.comments.summmary.total_count : 0
-      })
+        , link: item.link
+      }
+      if (card.type == 'photo') {
+        card.name = ''
+      }
+      data.push(card)
     }
   })
   data.sort((a, b) => {
     return b.likes - a.likes
   })
+  console.log(data);
   data.pop()
   data.map((card, i) => {
     $('#results').append($('<div/>', {
       class: 'card'
+    , "data-link": card.link
     , html: '<div class="image">                                               \
+        <i class="card-type '+card.type+' "></i>                               \
         <img src="'+card.img+'">                                               \
       </div>                                                                   \
       <div class="content">                                                    \
         <div class="header">'+card.name+'</div>                                \
         <div class="meta">                                                     \
-          <a>'+card.type+'</a>                                                 \
         </div>                                                                 \
         <div class="description">                                              \
         </div>                                                                 \
       </div>                                                                   \
       <div class="extra content">                                              \
         <span>                                                                 \
-          <i class="user icon"></i>                                            \
+          <i class="likes icon"></i>                                           \
+          '+card.likes+'                                                       \
+        </span>                                                                \
+        <span>                                                                 \
+          <i class="_comments icon"></i>                                       \
+          '+card.comments+'                                                    \
+        </span>                                                                \
+        <span>                                                                 \
+          <i class="shares icon"></i>                                          \
           '+card.shares+'                                                      \
         </span>                                                                \
       </div>'
+      , click: function(){
+          var win = window.open($(this).data('link'), '_blank');
+          win.focus();
+        }
     }))
   })
 }
