cf33a2f5f5a597228a0076d164bc2c8e7d339be1
Keiichi Lindley <digizn@gmail.com>
2016-05-26 22:10:04 -0600

    Implement WP API

--- public/js/articles.js
+++ public/js/articles.js
@@ -77,10 +77,6 @@ $(function(){
             method: 'DELETE',
             success: function(res){
               $(e.target).parent().parent().fadeOut(250).remove()
-              if (res.editLink) {
-                var win = window.open(res.editLink, '_blank');
-                win.focus();
-              }
             }
           });
         } else {
@@ -105,6 +101,10 @@ $(function(){
           method: 'DELETE',
           success: function(res){
             $(evt.target).parent().parent().fadeOut(250).remove()
+            if (res.editLink) {
+              var win = window.open(res.editLink, '_blank');
+              win.focus();
+            }
           }
         });
       }
